e2249a2aa42deeb33afb1fa2130181bc79e2f70c
Sam Panza <sam@panza>
2021-04-29 21:33:26 +0700

    booted with kafka

---
+++ src/main/java/zberpunx/lokafka/LokafkaSpringedApplication.java
@@ -0,0 +1,13 @@
+package zberpunx.lokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokafkaSpringedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LokafkaSpringedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zberpunx/lokafka/LokafkaSpringedApplicationTests.java
@@ -0,0 +1,13 @@
+package zberpunx.lokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokafkaSpringedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
