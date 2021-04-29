462e1f2c2eded3c6b70fb792e48dd6ae55a9f40c
Sam Panza <sam@panza>
2021-04-29 22:57:34 +0700

    ekb bean with default ports

--- src/main/java/zberpunx/lokafka/LokafkaSpringedApplication.java
+++ src/main/java/zberpunx/lokafka/LokafkaSpringedApplication.java
@@ -1,13 +1,22 @@
 package zberpunx.lokafka;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 
 @SpringBootApplication
 public class LokafkaSpringedApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LokafkaSpringedApplication.class, args);
     }
 
+    @Bean
+    EmbeddedKafkaBroker ekb(@Value("${zkPort:2181}") int zkPort,
+                            @Value("${kafkaPort:9092}") int kafkaPort) {
+        return new EmbeddedKafkaBroker(1)
+                .zkPort(zkPort)
+                .kafkaPorts(kafkaPort);
+    }
 }
